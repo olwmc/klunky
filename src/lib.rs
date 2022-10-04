5454e3aa2534913aeee74e77f7eb9e4ab9771c96
olwmc <olwmc@protonmail.ch>
2022-10-04 12:44:57 -0400

    Completely new scheme

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,63 @@
-/* This doesn't do what I want it to do!! */
-
+use std::io::Write;
+use std::net::{TcpListener, TcpStream, SocketAddr};
 use std::sync::{Arc, Mutex};
-use serde_derive::{Deserialize, Serialize};
-use warp::{Filter};
 
-#[derive(Deserialize, Serialize, Debug)]
-pub struct KlunkyRequest {
-    action: String,
-    params: Vec<String>,
-    key: Option<String>,
+struct KlunkyRequest;
+struct KlunkyConnection;
+
+/* Need to abstract away this shit and wrap stuff in a KlunkyRequest(inside tcp stream) and a KlunkyConnection(tcpstream itself) */
+struct KlunkyServer {
+    // need this to work between threads
+    conns: Arc<Mutex<Vec<(TcpStream, SocketAddr)>>>,
+    listener: TcpListener
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-pub struct KlunkyResponse {
-    response: Vec<String>,
-    error: Vec<String>,
-}
+impl KlunkyServer {
+    // Spawn => Add incoming connections to queue, lock
+    // connections: returns an iterator
 
-pub type Prox<T> = Arc<Mutex<T>>;
+    fn new(port: u32) -> Self {
+        let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+        listener.set_nonblocking(true).expect("Cannot set non-blocking");
 
-pub fn with_prox<T: Send>(prox:Prox<T>) -> impl Filter<Extract = (Prox<T>,), Error = std::convert::Infallible> + Clone {
-    warp::any().map(move || prox.clone())
-}
-
-pub fn klunky_spawn<T: Send + 'static>(proxy: Prox<T>, f: fn(KlunkyRequest, Prox<T>) -> String) {
-     let make_request = warp::post()
-            .and(warp::body::json())
-            .and(with_prox(Arc::clone(&proxy)))
-            .map(f);
-
-    tokio::spawn(async move {
-        warp::serve(make_request).run(([127, 0, 0, 1], 55865)).await
-    });
-}
-
-impl From<KlunkyResponse> for String {
-    fn from(resp: KlunkyResponse) -> Self {
-        serde_json::to_string(&resp).unwrap()
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use reqwest;
-
-    pub struct App {
-        words: Vec<String>,
+        Self { conns: Default::default(), listener }
     }
 
-    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-    async fn test_basic_app() {
-        let app = App {words: vec![] };
-        let proxy_truth = Arc::new(Mutex::new(app));
-        let proxy: Prox<App> = proxy_truth.clone();
-
-        klunky_spawn(proxy.clone(), |req, p| {
-            let mut pp = p.lock().unwrap();
-            pp.words.push(req.action.clone());
-
-            KlunkyResponse { response: vec![format!("added word: {}", req.action)], error: vec![]}.into()
+    fn spawn(&mut self) {
+        let copy = self.conns.clone();
+        thread::spawn(move || {
+            
         });
-
-        for i in 0..5 {
-            reqwest::Client::new().post("http://127.0.0.1:55865")
-                .body(format!("{{\"action\": \"test{}\", \"params\": []}}", i))
-                .send()
-                .await.unwrap();
-        }
-
-        let proxy = proxy_truth.clone();
-        let pp = proxy.lock().unwrap();
-        assert_eq!(pp.words, vec!["test0","test1","test2","test3","test4"])
     }
+    //fn connections(&mut self) <== this also clears out the connections
+}
+use std::{thread, time};
+
+fn main() {
+    let mut kc = KlunkyServer::new(6666);
+    kc.spawn();
+    
+    // loop {
+    //     // Work we're doing in the middle here
+    //     thread::sleep(time::Duration::from_millis(1000));
+
+    //     // ---
+    //     // ---
+    //     // ---
+    //     // ---
+        
+    //     // Accept connections
+    //     match kc.listener.accept() {
+    //         Ok( (_socket, _addr) ) => kc.conns.push( (_socket, _addr) ),
+    //         Err(_) => {}
+    //     }
+
+    //     // Process connections
+    //     for conn in &mut kc.conns {
+    //         let buf = "abcde".as_bytes();
+    //         conn.0.write(&buf);
+    //     }
+
+    //     kc.conns.clear();
+    // }
 }
